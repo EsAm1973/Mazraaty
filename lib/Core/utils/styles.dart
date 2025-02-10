e6be10d9a0dd3f97e80a4451c5761a680e10594a
EsAm1973 <me354172@gmail.com>
2025-02-10 21:55:31 +0200

    Make the font styles of the app

---
+++ lib/Core/utils/styles.dart
@@ -0,0 +1,44 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const textStyle50 = TextStyle(
+    fontSize: 50,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const textStyle38 = TextStyle(
+    fontSize: 38,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const textStyle30 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const textStyle26 = TextStyle(
+    fontSize: 26,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const textStyle18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const textStyle15 = TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const textStyle13 = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:mazraaty/home.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mazraaty/constants.dart';
+//import 'package:mazraaty/home.dart';
+import 'package:mazraaty/Features/splash/presentation/views/splash_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,32 +11,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      title: 'Mazraaty',
+      theme: ThemeData.light().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+        textTheme: GoogleFonts.inderTextTheme(),
       ),
-      home:  HomeScreen(),
+      home: const SplashView(),
     );
   }
 }
-
