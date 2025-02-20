8797b94bf36521f1723f8a33df8db1c3177b0c1d
EsAm1973 <me354172@gmail.com>
2025-02-20 18:50:00 +0200

    create login button and make validate

--- lib/Features/authentication/presentation/views/widgets/login_button.dart
+++ lib/Features/authentication/presentation/views/widgets/login_button.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mazraaty/Core/widgets/custom_button.dart';
 
 class LoginButton extends StatelessWidget {
-  const LoginButton({super.key, this.onPressed});
+  const LoginButton({super.key, required this.onPressed});
   final VoidCallback? onPressed;
   @override
   Widget build(BuildContext context) {
--- lib/Features/authentication/presentation/views/widgets/login_view_body.dart
+++ lib/Features/authentication/presentation/views/widgets/login_view_body.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mazraaty/Core/widgets/custom_button.dart';
 import 'package:mazraaty/Features/authentication/presentation/views/widgets/login_button.dart';
 import 'package:mazraaty/Features/authentication/presentation/views/widgets/login_forgetpass.dart';
 import 'package:mazraaty/Features/authentication/presentation/views/widgets/login_name_textfeild.dart';
@@ -11,31 +10,48 @@ class LoginViewBody extends StatelessWidget {
   LoginViewBody({super.key});
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const LoginTopImage(),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              const LoginWelcomeMessage(),
-              const SizedBox(height: 20),
-              LoginEmailTextFeild(
-                emailController: emailController,
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const LoginTopImage(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  const LoginWelcomeMessage(),
+                  const SizedBox(height: 20),
+                  LoginEmailTextFeild(
+                    emailController: emailController,
+                  ),
+                  const SizedBox(height: 20),
+                  LoginPasswordTextFeild(
+                    passwordController: passwordController,
+                  ),
+                  const LoginForgetPassword(),
+                  const SizedBox(height: 30),
+                  LoginButton(
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        // Navigate to main screen
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('Login successful!'),
+                          ),
+                        );
+                      }
+                    },
+                  ),
+                ],
               ),
-              const SizedBox(height: 20),
-              LoginPasswordTextFeild(
-                passwordController: passwordController,
-              ),
-              const LoginForgetPassword(),
-              const SizedBox(height: 20),
-              const LoginButton(),
-            ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
