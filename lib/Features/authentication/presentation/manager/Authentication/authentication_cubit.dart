0b4b99f4bccb77b649c7491a9557d35e75a2b4f9
EsAm1973 <me354172@gmail.com>
2025-02-21 20:34:47 +0200

    create authentication state and cubit

--- lib/Features/authentication/presentation/manager/Authentication/authentication_cubit.dart
+++ lib/Features/authentication/presentation/manager/Authentication/authentication_cubit.dart
@@ -1,8 +1,40 @@
 import 'package:bloc/bloc.dart';
+import 'package:mazraaty/Core/models/user.dart';
+import 'package:mazraaty/Features/authentication/data/repos/authentication_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'authentication_state.dart';
 
 class AuthenticationCubit extends Cubit<AuthenticationState> {
-  AuthenticationCubit() : super(AuthenticationInitial());
+  AuthenticationCubit(this.authenticationRepo) : super(AuthenticationInitial());
+  final AuthenticationRepo authenticationRepo;
+
+  Future<void> login({required String email, required String password}) async {
+    emit(LoginAuthLoading());
+    final result =
+        await authenticationRepo.login(email: email, password: password);
+    result.fold((l) => {emit(LoginAuthError(errorMessage: l.errorMessage))},
+        (r) => {emit(LoginAuthSuccess(user: User.fromJson(r)))});
+  }
+
+  Future<void> register({
+    required String name,
+    required String phone,
+    required String email,
+    required String password,
+  }) async {
+    emit(RegisterAuthLoading());
+    final result = await authenticationRepo.register(
+      username: name,
+      phone: phone,
+      email: email,
+      password: password,
+    );
+    result.fold(
+        (failure) =>
+            emit(RegisterAuthError(errorMessage: failure.errorMessage)),
+        (user) {
+      emit(RegisterAuthSuccess(user: User.fromJson(user)));
+    });
+  }
 }
--- lib/Features/authentication/presentation/manager/Authentication/authentication_state.dart
+++ lib/Features/authentication/presentation/manager/Authentication/authentication_state.dart
@@ -4,3 +4,31 @@ part of 'authentication_cubit.dart';
 sealed class AuthenticationState {}
 
 final class AuthenticationInitial extends AuthenticationState {}
+
+final class LoginAuthLoading extends AuthenticationState {}
+
+final class LoginAuthSuccess extends AuthenticationState {
+  final User user;
+
+  LoginAuthSuccess({required this.user});
+}
+
+final class LoginAuthError extends AuthenticationState {
+  final String errorMessage;
+
+  LoginAuthError({required this.errorMessage});
+}
+
+final class RegisterAuthLoading extends AuthenticationState {}
+
+final class RegisterAuthSuccess extends AuthenticationState {
+  final User user;
+
+  RegisterAuthSuccess({required this.user});
+}
+
+final class RegisterAuthError extends AuthenticationState {
+  final String errorMessage;
+
+  RegisterAuthError({required this.errorMessage});
+}
