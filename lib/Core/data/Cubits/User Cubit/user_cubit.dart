89bb7e990d4864b49b21a7d5c71116fa12824484
EsAm1973 <me354172@gmail.com>
2025-03-06 22:23:41 +0200

    create user cubit and user states

--- lib/Core/data/Cubits/User Cubit/user_cubit.dart
+++ lib/Core/data/Cubits/User Cubit/user_cubit.dart
@@ -1,8 +1,42 @@
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:mazraaty/Core/data/repository/User%20Repository/user_repo.dart';
 import 'package:mazraaty/Core/models/user.dart';
 
 part 'user_state.dart';
 
 class UserCubit extends Cubit<UserState> {
-  UserCubit() : super(UserInitial());
+  final IUserRepository userRepository;
+
+  UserCubit({required this.userRepository}) : super(UserInitial());
+
+  User? get currentUser {
+    if (state is UserLoaded) {
+      return (state as UserLoaded).user;
+    }
+    return null;
+  }
+
+  Future<void> loadUser() async {
+    try {
+      emit(UserLoading());
+      final user = await userRepository.getUser();
+      if (user != null) {
+        emit(UserLoaded(user));
+      } else {
+        emit(UserInitial());
+      }
+    } catch (e) {
+      emit(UserError(e.toString()));
+    }
+  }
+
+  Future<void> saveUser(User user) async {
+    try {
+      await userRepository.saveUser(user);
+      emit(UserLoaded(user));
+    } catch (e) {
+      emit(UserError(e.toString()));
+    }
+  }
 }
