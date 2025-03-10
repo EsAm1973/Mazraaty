4d0c9dce16c88ae0040e1525d36738c5cee2feab
EsAm1973 <me354172@gmail.com>
2025-03-10 07:08:00 +0200

    create details app bar

---
+++ lib/Features/plant_library/presentation/views/widgets/details_appbar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:mazraaty/Core/utils/styles.dart';
+import 'package:mazraaty/constants.dart';
+
+class DetailsCustomAppBar extends StatelessWidget {
+  const DetailsCustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 25, right: 27, left: 27),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Icon(
+            Icons.arrow_back_ios,
+            color: kMainColor,
+            size: 30,
+          ),
+          Text(
+            'Tomato',
+            style: Styles.textStyle34.copyWith(
+              fontFamily: kfontFamily,
+              color: kMainColor,
+            ),
+          ),
+          Container(
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              color: kMainColor,
+            ),
+            child: const Icon(
+              Icons.favorite_border,
+              color: Colors.white,
+              size: 30,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
