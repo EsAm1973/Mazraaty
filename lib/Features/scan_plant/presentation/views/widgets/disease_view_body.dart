0329ad0069209b5616b6caa85a6d26465bd0095b
EsAm1973 <me354172@gmail.com>
2025-03-17 22:10:15 +0200

    create title of the disease

--- lib/Features/scan_plant/presentation/views/widgets/disease_view_body.dart
+++ lib/Features/scan_plant/presentation/views/widgets/disease_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mazraaty/Core/widgets/sticky_headers_delegate.dart';
 import 'package:mazraaty/Features/scan_plant/presentation/views/widgets/disease_top_image.dart';
 
@@ -17,7 +18,31 @@ class DiseaseViewBody extends StatelessWidget {
             maxExtent: 300,
             minExtent: 300,
           ),
-        )
+        ),
+        SliverList(
+          delegate: SliverChildListDelegate([
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    'Mint Rust',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  
+                ],
+              ),
+            ),
+          ]),
+        ),
       ],
     );
   }
