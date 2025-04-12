222daa357f98f9f6b071e402235840d81ad02ddb
EsAm1973 <me354172@gmail.com>
2025-04-12 22:15:37 +0200

    Add dependancy in pubspec yaml

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const String baseUrl = 'https://c1fb-154-238-236-36.ngrok-free.app/api/';
+const String baseUrl = 'https://d8cd-154-237-96-151.ngrok-free.app/api/';
 const Color kPrimaryColor = Colors.white;
 const Color kMainColor = Color(0xff3E7B27);
 const Color kScaffoldColor = Color(0xffF1F1F1);
